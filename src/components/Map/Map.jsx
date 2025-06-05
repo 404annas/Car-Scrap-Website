34a4be393cba1fe1da0a7d3b82d50810fa9c8b5d
Muhammad Annas Khan <annasking601@gmail.com>
2025-06-05 11:17:21 +0500

    Changes 1

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -12,7 +12,7 @@ const Map = () => {
       <img
         src={MapImage}
         alt="City Map"
-        className="w-full h-[60vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] object-cover px-2"
+        className="w-fit mx-auto h-[60vh] sm:h-[70vh] md:h-[80vh] object-cover px-2"
       />
     </section>
   );
